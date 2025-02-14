d8c95252a97a3cd8757ac26ebe8141f8e409a289
Vsevolod T. <74288513+NikWither@users.noreply.github.com>
2025-02-14 17:12:07 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -3,21 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\V1\LessonController;
-use App\Http\Controllers\API\V1\LessonsBySubjectsController;
 use App\Http\Controllers\API\V1\SubjectsController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/getSDF', function() {
-    dd('awedasd');
-});
-
 Route::prefix('v1')->group(function () {
-
-    Route::get('/lessons/by-subject', LessonsBySubjectsController::class);
-
+    
     Route::apiResource('lessons', LessonController::class);
 
     Route::apiResource('subjects', SubjectsController::class);
